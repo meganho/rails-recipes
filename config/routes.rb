8fd056dc931ad373a51a62256ce2311f1ad45c37
meganho <bbeeaanns@gmail.com>
2017-07-17 16:56:42 +0200

    ch10

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :events do
-      resources :tickets, :controller => "event_tickets"
-  resource :user
-end
+  resources :events
+resource :user
 
 get "/faq" => "pages#faq"
 
 namespace :admin do
     root "events#index"
-    resources :events
-     resources :users do
-       resource :profile, :controller => "user_profiles"
-     end
- end
+    resources :events do
+      resources :tickets, :controller => "event_tickets"
+    end
+    resources :users
+end
 
   root "events#index"
 end
